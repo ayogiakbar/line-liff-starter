ceb2f067c75cbe45721da07cb3a7a5919881810d
mosn-bw <44426932+mosn-bw@users.noreply.github.com>
2018-11-19 08:34:41 +0330

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -62,7 +62,24 @@ function initializeApp(data) {
         });
     });
 }
-
+{
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+    "type": "buttons",
+    "actions": [
+      null,
+      {
+        "type": "message",
+        "label": "Action 2",
+        "text": "Action 2"
+      }
+    ],
+    "thumbnailImageUrl": "http://s8.picofile.com/file/8343114800/1541846041064.png",
+    "title": "Title",
+    "text": "mosen"
+  }
+}
 function toggleProfileData() {
     var elem = document.getElementById('profileinfo');
     if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
